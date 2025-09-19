fec13373a31546969af07a54abad2e65edc1844e
bhumika9385 <165877914+bhumika9385@users.noreply.github.com>
2025-09-19 16:13:28 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,10 @@
-// HelloWorld.java
-class demo {
+
+// Addition.java
+class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int a = 10;   // first number
+        int b = 20;   // second number
+        int sum = a + b;  // addition
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
     }
 }
